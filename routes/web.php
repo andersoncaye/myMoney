e5f9f86abd87fec751429399096f93b91dd08648
Anderson Caye <andersoncaye@gmail.com>
2021-03-28 20:41:48 -0300

    Criado o Login do Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+    /*Route::get('/', function () {
+        return view('welcome');
+    });*/
+
+    Route::get('/', 'HomeController@index');
+    Auth::routes();
+    Route::get('/home', 'HomeController@index')->name('home');
 });
